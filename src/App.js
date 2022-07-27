2b370fa48b9db53f9c3d588a1dbcb4c2bd24c016
Artur Przystaś <artek.przystas@gmail.com>
2022-07-27 21:29:14 +0200

    flipping cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import SingleCard from "./compontents/singleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/sword-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/potion-1.png" },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
 ];
 
 function App() {
@@ -43,14 +43,23 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("THEY MATCH!!!");
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
       } else {
-        console.log("they don't match :(");
-        resetTurn();
+        setTimeout(() => resetTurn(), 1202);
       }
     }
   }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
 
   return (
     <div className="App">
@@ -59,7 +68,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
--- src/compontents/singleCard.js
+++ src/compontents/singleCard.js
@@ -1,13 +1,13 @@
 import "./SingleCard.css";
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   };
 
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img src={card.src} className="front" alt="card front" />
         <img
           src="/img/cover.png"
